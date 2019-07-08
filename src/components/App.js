a38064c39b3f6d09d74e6b1655f5d14429945884
Yuri Dmitri Dubler <yuridmitridubler@Yuris-MacBook-Pro.local>
2019-07-08 12:44:20 -0600

    Added npm package that detects mobile or browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,107 @@
 // React
-import React, { useState } from "react";
+import React from "react";
 
 // Aphrodite
 import { StyleSheet, css } from "aphrodite";
 
-// App Function Component
-export default function App() {
-  const [count, setCount] = useState(0);
-  const [strokeColor, setStrokeColor] = useState("white");
+// React-Device-Detect
+// This library is used to detect whether or not the user is on a mobile device
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+  browserName
+} from "react-device-detect";
 
-  return (
-    <div>
-      This is a Server-Side-Rendered React App.
-      <br />
-      CSS content is served server-side using Aphrodite.
-      <br />
-      <br />
-      Here is a stateful button:
-      <br />
-      <button
-        className={css(styles.red, styles.shake)}
-        onClick={() => setCount(count + 1)}
-      >
-        {count}
-      </button>
-      <br />
-      <br />
-      <svg x="50" height="200px" width="200px">
-        <rect width="100%" height="100%" fill="lightgray" />
-        <circle
-          className={css(styles.colorTrans)}
-          r="5px"
-          cx="50%"
-          cy="50%"
-          stroke={strokeColor}
-          onMouseOver={() => {
-            setStrokeColor("red");
-          }}
-          onMouseLeave={() => {
-            setStrokeColor("white");
-          }}
-        />
-      </svg>
-    </div>
-  );
+// App Function Component
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orientation: "portrait",
+      touchscreen: true,
+      count: 1,
+      strokeColor: "white"
+    };
+
+    this.incrementCount = this.incrementCount.bind(this);
+    this.setStrokeColor = this.setStrokeColor.bind(this);
+  }
+
+  componentDidMount() {
+    // if (window.matchMedia("(orientation: portrait)").matches) {
+    //   console.log("orientation: portrait");
+    //   this.setState({ orientation: "portrait" });
+    // } else {
+    //   console.log("orientation: landscape");
+    //   this.setState({ orientation: "landscape" });
+    // }
+    console.log("screen orientation: " + window.screen.orientation.type);
+    this.setState({ orientation: window.screen.orientation.type });
+
+    console.log("touchscreen: " + "ontouchstart" in window);
+    if ("ontouchstart" in window) {
+      this.setState({ touchscreen: true });
+    } else {
+      this.setState({ touchscreen: false });
+    }
+  }
+
+  incrementCount(increment) {
+    this.setState(prevState => {
+      return { count: prevState.count + increment };
+    });
+  }
+
+  setStrokeColor(colorIn) {
+    this.setState({ strokeColor: colorIn });
+  }
+
+  render() {
+    return (
+      <div>
+        This is a Server-Side-Rendered React App.
+        <br />
+        CSS content is served server-side using Aphrodite.
+        <br />
+        Device is detected using react-device-detect:
+        <br />
+        &nbsp;&nbsp;&nbsp;Browser: {browserName}
+        <br />
+        &nbsp;&nbsp;&nbsp;Mobile or Browser: {isMobile && "Mobile"}
+        {isBrowser && "Browser"}
+        <br />
+        &nbsp;&nbsp;&nbsp;Orientation: {this.state.orientation}
+        <br />
+        &nbsp;&nbsp;&nbsp;Touch-Enabled: {this.state.touchscreen.toString()}
+        <br />
+        Here is a stateful button (doubles the value):
+        <br />
+        <button
+          className={css(styles.red, styles.shake)}
+          onClick={() => this.incrementCount(this.state.count)}
+        >
+          {this.state.count}
+        </button>
+        <br />
+        <br />
+        <svg x="50" height="200px" width="200px">
+          <rect width="100%" height="100%" fill="lightgray" />
+          <circle
+            className={css(styles.colorTrans)}
+            r="5px"
+            cx="50%"
+            cy="50%"
+            stroke={this.state.strokeColor}
+            onMouseOver={() => this.setStrokeColor("red")}
+            onMouseLeave={() => this.setStrokeColor("white")}
+          />
+        </svg>
+      </div>
+    );
+  }
 }
 
 // Aphrodite Stylesheet
